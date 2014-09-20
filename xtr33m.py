a368d926207af6c8bb9cce871a8231c73b91b1f7
tobias <tobias.perelstein@gmail.com>
2014-09-20 05:13:27 -0400

    parsing result and storing band data into files

--- xtr33m.py
+++ xtr33m.py
@@ -1,13 +1,16 @@
 import collections
 import requests
+import os
 from bs4 import BeautifulSoup
 
-#This function gets the json for every letter band on The Metal Archives. Each json request returns a list of 500 bands. Every list is paired with a key representing the letter
+#This function gets the json for every letter band on The Metal Archives. Each json request returns a list of 500 bands.
+#Every list is paired with a key representing the letter
 #Once every list of bands is retrieved for the letter, the process is repeated for bands #-Z
 #The result in the form: 'letter': [[list of bands],[list of bands]....]
-def get_all_bands():
+def build_band_list():
     result = {}
-    letters = ['NBR','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    #letters = ['NBR','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    letters = ['NBR']
     index = 0
     count = 0
 
@@ -28,31 +31,28 @@ def get_all_bands():
                     count = 0
 
     return result
-'''
-def map_bands():
-    result = {}
 
-    resp = requests.get('http://www.metal-archives.com/browse/ajax-letter/l/A/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart='+count+'&iDisplayLength=&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&_=1410751488887')
-    print len(resp.json()['aaData'])
+def get_band_info():
+    result = build_band_list()
 
-    for key in resp.json():
-        if isinstance(resp.json()[key],collections.Iterable):
-            for list in resp.json()[key]:
-                print len(list)
-                soup = BeautifulSoup(list[0])
-                band = soup.get_text()
-                #print band
+    for letter in result:
+        try:
+            os.makedirs(letter)
+        except OSError:
+            if not os.path.isdir(letter):
+                raise
+        for band_list in result[letter]:
+            for i in range (0,len(band_list)):
+                soup = BeautifulSoup((band_list[i][0]))
+                band_name = soup.get_text()
+                print band_name
                 for child in soup.findAll('a'):
                     link = child.get('href')
-                    #result[band] = link
+                    print link
+                    file_name = os.path.join(letter+'/', str(band_name)+".html")
+                    band_file = open(file_name, "w")
+                    to_file = requests.get(link).content
+                    band_file.write(to_file)
+                    band_file.close()
 
-    return result
-
-result = map_bands()
-for band in result:
-    print band+'\n'+result[band]+'\n\n'
-'''
-result = get_all_bands()
-print result
-
-
+get_band_info()
