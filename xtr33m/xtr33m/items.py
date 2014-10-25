25aa00024e5b02c0bd2ce796e225eec463ee0245
tobias <tobias.perelstein@gmail.com>
2014-10-25 03:50:17 -0400

    Parsing band pages from single json request and outputting parsed json

--- xtr33m/xtr33m/items.py
+++ xtr33m/xtr33m/items.py
@@ -7,32 +7,33 @@
 
 import scrapy
 
-
 class band_item(scrapy.Item):
-    # define the fields for your item here like:
+    # define the Fields for your item here like:
     # name = scrapy.Field()
     name = scrapy.Field()
     id = scrapy.Field()
+    link = scrapy.Field
     description = scrapy.Field()
-    country = scrapy.field()
-    location = scrapy.field()
-    status = scrapy.field()
-    formation_year = scrapy.field()
-    genre = scrapy.field()
-    current_label = scrapy.field()
-    years_active = scrapy.field()
-    complete_lineup = scrapy.field()
-    current_lineup = scrapy.field()
-    past_lineup = scrapy.field()
-    live_lineup = scrapy.field()
-    official_links = scrapy.field()
-    official_merch = scrapy.field()
-    unofficial_links= scrapy.field()
-    band_label_links = scrapy.field()
-    band_tabs = scrapy.field()
-    similar_artists = scrapy.field()
+    country = scrapy.Field()
+    location = scrapy.Field()
+    status = scrapy.Field()
+    formation_year = scrapy.Field()
+    lyrical_themes = scrapy.Field()
+    genre = scrapy.Field()
+    current_label = scrapy.Field()
+    years_active = scrapy.Field()
+    complete_lineup = scrapy.Field()
+    current_lineup = scrapy.Field()
+    past_lineup = scrapy.Field()
+    live_lineup = scrapy.Field()
+    official_links = scrapy.Field()
+    official_merch = scrapy.Field()
+    unofficial_links= scrapy.Field()
+    band_label_links = scrapy.Field()
+    band_tabs = scrapy.Field()
+    similar_artists = scrapy.Field()
     #{releases: {'all': name: {tracks: {track_name,tracknum_length,lyrics} ,type,year,notes,{album_lineup: name,role}
-    releases = scrapy.field()
+    releases = scrapy.Field()
 
 
 
--- xtr33m/xtr33m/spiders/ma_spider.py
+++ xtr33m/xtr33m/spiders/ma_spider.py
@@ -1,15 +1,75 @@
 import scrapy
+import json
 from xtr33m.items import band_item
+from bs4 import BeautifulSoup
 
 class ma_spider(scrapy.Spider):
     name = "ma"
-    allowed_domains = ["http://www.metal-archives.com"]
-    start_urls = []
+    allowed_domains = ["www.metal-archives.com"]
+    start_urls = ['http://www.metal-archives.com/browse/ajax-letter/l/NBR/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart=0&iDisplayLength=500&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&_=1414220342814']
 
-    def parse_ajax(self, response):
-        pass
+    def parse(self, response):
+        jsonresponse = json.loads(response.body)
+        soup = BeautifulSoup(jsonresponse["aaData"][0][0])
+        for item in range(0,len(jsonresponse["aaData"])):
+            soup = BeautifulSoup(jsonresponse["aaData"][item][0])
+            band_link = soup.select('a')[0]['href']
+            print 'yielding %s: ' % band_link
+            print scrapy.Request(band_link,callback=self.parse_band)
+            yield scrapy.Request(band_link,callback=self.parse_band)
+
     def parse_band(self,response):
-        pass
+        print 'asdf'
+        soup = BeautifulSoup(response.body)
+        item = band_item()
+
+        band_name = soup.select('.band_name')[0].text
+        band_id = response.url.split('/')[-1]
+        country = soup.select('#band_stats dd:nth-of-type(1) a')[0].text
+        location = soup.select('#band_stats dd:nth-of-type(2)')[0].text
+        status = soup.select('#band_stats dd:nth-of-type(3)')[0].text
+        formation = soup.select('#band_stats dd:nth-of-type(4)')[0].text
+        genre = soup.select('#band_stats dd:nth-of-type(5)')[0].text
+        lyrical_themes = soup.select('#band_stats dd:nth-of-type(6)')[0].text
+        current_label = soup.select('#band_stats dd:nth-of-type(7)')[0].text
+        years_active = soup.select('#band_stats dd:nth-of-type(8)')[0].text
+        item['name'] = band_name
+        item['id'] = band_id
+        item['country'] =country
+        item['location'] = location
+        item['status'] = status
+        item['formation_year'] = formation
+        item['genre'] = genre
+        item['lyrical_themes'] =lyrical_themes
+        item['current_label'] = current_label
+        item['years_active'] = years_active
+        print 'printing the item: %s: ' % item
+        yield item
+
+        #Need to figure out how to nest items
+        #if soup.find(id='band_tab_members_all') is not None:
+        #    #All of the role info is a sibling to the band member itself
+        #    #complete linup
+        #    lineup = soup.select('#band_tab_members_all .lineupRow td a')
+        #    roles = soup.select('.lineupRow td ~ td')
+        #    for member,role, in zip(lineup,roles):
+        #        band_member = member.text+' - '+role.text.strip()
+        #    #current linup
+        #    lineup = soup.select('#band_tab_members_current .lineupRow td a')
+        #    roles = soup.select('.lineupRow td ~ td')
+        #    for member,role, in zip(lineup,roles):
+        #        band_member = member.text+' - '+role.text.strip()
+        #    #past lineup
+        #    lineup = soup.select('#band_tab_members_past .lineupRow td a')
+        #    roles = soup.select('.lineupRow td ~ td')
+        #    for member,role, in zip(lineup,roles):
+        #        band_member = member.text+' - '+role.text.strip()
+        #    #live lineup
+        #    lineup = soup.select('#band_tab_members_live .lineupRow td a')
+        #    roles = soup.select('.lineupRow td ~ td')
+        #    for member,role, in zip(lineup,roles):
+        #        band_member = member.text+' - '+role.text.strip()
+
     def parse_description(self,response):
         pass
     def parse_similar_artists(self,response):
