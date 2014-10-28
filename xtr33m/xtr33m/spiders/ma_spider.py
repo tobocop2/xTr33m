51a549e43d36a2b609296f4b75d7f655fd83ece1
Tobias <tobias@perelste.in>
2014-10-28 09:04:55 +0000

    Scraping all urls now

--- xtr33m/xtr33m/spiders/ma_spider.py
+++ xtr33m/xtr33m/spiders/ma_spider.py
@@ -1,38 +1,79 @@
-import scrapy
-import json
+from scrapy.spider import Spider
+from scrapy.http import Request
 from xtr33m.items import band_item
 from bs4 import BeautifulSoup
+import string
+import time
+import json
 
-class ma_spider(scrapy.Spider):
+START_URL_FMT = 'http://www.metal-archives.com/browse/ajax-letter/l/{}/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart=0&iDisplayLength=500&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&_={}'
+NEXT_URL_FMT = 'http://www.metal-archives.com/browse/ajax-letter/l/{}/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart={}&iDisplayLength=500&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&_={}'
+
+class ma_spider(Spider):
     name = "ma"
     allowed_domains = ["www.metal-archives.com"]
-    start_urls = ['http://www.metal-archives.com/browse/ajax-letter/l/NBR/json/1?sEcho=1&iColumns=4&sColumns=&iDisplayStart=0&iDisplayLength=500&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&_=1414220342814']
 
-    def parse(self, response):
+    def start_requests(self):
+        #letters = ['NBR']+list(string.uppercase)
+        letters = ['J']
+        #for letter in letters:
+        #    #passing in the letter and the time into the url
+        #    url = START_URL_FMT.format(letter,int(time.time()))
+        #    #using this for the next url format
+        #    meta = {'letter': letter}
+        #    yield Request(url,callback=self.parse_json,meta=meta)
+        for letter in letters:
+            #passing in the letter and the time into the url
+            url = START_URL_FMT.format(letter,int(time.time()))
+            #using this for the next url format
+            meta = {'letter': letter}
+            yield Request(url,callback=self.parse_first,meta=meta)
+
+
+    def parse_first(self, response):
         jsonresponse = json.loads(response.body)
-        soup = BeautifulSoup(jsonresponse["aaData"][0][0])
+        total = jsonresponse['iTotalRecords']
+        display = 500
+        current = 0
+        #if total > display:
+        #   remain = total - display
+        #   while current < remain:
+        #       # yield requests with..
+        #       current += display
+        #       url = NEXT_URL_FMT.format(response.meta['letter'], current, int(time.time()))
+        #       print url
+        #       yield Request(url, callback=self.parse_json)
+        for i in range(0,total,500):
+            url = NEXT_URL_FMT.format(response.meta['letter'], i, int(time.time()))
+            print url
+            yield Request(url, callback=self.parse_json)
+
+
+    def parse_json(self,response):
+        jsonresponse = json.loads(response.body)
         for item in range(0,len(jsonresponse["aaData"])):
             soup = BeautifulSoup(jsonresponse["aaData"][item][0])
             band_link = soup.select('a')[0]['href']
             print 'yielding %s: ' % band_link
-            print scrapy.Request(band_link,callback=self.parse_band)
-            yield scrapy.Request(band_link,callback=self.parse_band)
+            print Request(band_link,callback=self.parse_band)
+            yield Request(band_link,callback=self.parse_band)
 
     def parse_band(self,response):
-        print 'asdf'
         soup = BeautifulSoup(response.body)
         item = band_item()
 
         band_name = soup.select('.band_name')[0].text
+        print band_name
         band_id = response.url.split('/')[-1]
-        country = soup.select('#band_stats dd:nth-of-type(1) a')[0].text
-        location = soup.select('#band_stats dd:nth-of-type(2)')[0].text
-        status = soup.select('#band_stats dd:nth-of-type(3)')[0].text
-        formation = soup.select('#band_stats dd:nth-of-type(4)')[0].text
-        genre = soup.select('#band_stats dd:nth-of-type(5)')[0].text
-        lyrical_themes = soup.select('#band_stats dd:nth-of-type(6)')[0].text
-        current_label = soup.select('#band_stats dd:nth-of-type(7)')[0].text
-        years_active = soup.select('#band_stats dd:nth-of-type(8)')[0].text
+        country = soup.select('#band_stats dd')[0].text
+        location = soup.select('#band_stats dd')[1].text
+        status = soup.select('#band_stats dd')[2].text
+        formation = soup.select('#band_stats dd')[3].text
+        genre = soup.select('#band_stats dd')[4].text
+        lyrical_themes = soup.select('#band_stats dd')[5].text
+        current_label = soup.select('#band_stats dd')[6].text
+        years_active = soup.select('#band_stats dd')[7].text
+        years_active = ''.join([c for c in years_active if c not in '\n\t '])
         item['name'] = band_name
         item['id'] = band_id
         item['country'] =country
